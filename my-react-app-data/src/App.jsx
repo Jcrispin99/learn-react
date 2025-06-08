7009fffabab9e3420134de8150c64729055e1e82
Jhamil Antony Crispin Martel <wild@MacBook-Pro-de-Jhamil.local>
2025-06-08 00:24:24 -0500

    Creación de componente

--- my-react-app-data/src/App.jsx
+++ my-react-app-data/src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import StaticComponent from './assets/components/StaticComponent'
 
 function App() {
+  const items = ["Manzana", "Banana", "Pera", "Uva"];
   
   return (
-    <>
-      <StaticComponent>
-        
-      </StaticComponent>
-    </>
+    <section>
+      <h1>HOla!</h1>
+      <ul>
+        {
+          items.map((iten, index) => (
+            <li key={index}>{iten}</li>
+          ))
+        }
+      </ul>
+    </section>
   )
 }
 
